08b96a5bdd456212f33fca51ea1ac7cec7c8fb2f
Sergey Yurovskiy <sergeyy-sergeyyy@yandex.ru>
2025-09-14 21:12:43 +0300

    Sprint 13. add-reviews branch

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -62,10 +62,8 @@ CREATE TABLE IF NOT EXISTS reviews (
 
 -- Функциональность Лайки / Дислайки (Спринт 13)
 CREATE TABLE IF NOT EXISTS review_likes (
-    review_id BIGINT NOT NULL,
-    user_id BIGINT NOT NULL,
+    review_id BIGINT NOT NULL REFERENCES reviews(review_id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     is_like BOOLEAN NOT NULL,
-    PRIMARY KEY (review_id, user_id),
-    FOREIGN KEY (review_id) REFERENCES reviews(review_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    PRIMARY KEY (review_id, user_id)
 );
