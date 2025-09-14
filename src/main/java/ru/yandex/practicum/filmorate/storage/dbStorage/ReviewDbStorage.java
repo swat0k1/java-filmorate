56abeef2da1ca6106a312ef16dc54621fcd0b956
Sergey Yurovskiy <sergeyy-sergeyyy@yandex.ru>
2025-09-14 20:26:45 +0300

    Sprint 13. add-reviews branch

--- src/main/java/ru/yandex/practicum/filmorate/storage/dbStorage/ReviewDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dbStorage/ReviewDbStorage.java
@@ -6,15 +6,9 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.FindingException;
 import ru.yandex.practicum.filmorate.exception.InternalServerException;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.storage.interfaces.ReviewStorage;
 
-import java.sql.Timestamp;
-import java.util.Collection;
-import java.util.LinkedHashSet;
 import java.util.List;
 
 @Slf4j
