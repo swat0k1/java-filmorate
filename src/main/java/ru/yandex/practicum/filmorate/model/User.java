2bcde626e8e57b64061798684d12573fd5621864
Sergey Yurovskiy <145047682+swat0k1@users.noreply.github.com>
2025-09-15 13:39:26 +0300

    Sprint 13. Reviews feature was added
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Sprint 13. add-reviews branch
    
    * Code clean up and fix. Sprint 13. add-reviews branch

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -29,13 +29,4 @@ public class User {
 
     private Set<Integer> friends = new HashSet<>();
 
-    /*
-    public User(int id, String email, String login, String name, LocalDate birthday) {
-        this.id = id;
-        this.email = email;
-        this.login = login;
-        this.name = name;
-        this.birthday = birthday;
-    }
-     */
 }
