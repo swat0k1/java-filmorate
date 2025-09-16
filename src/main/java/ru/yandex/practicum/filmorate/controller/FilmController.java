28fafdbf6706c487ea06c574a90dda7b18962018
unknown <sergejurovskij82305@gmail.com>
2025-09-16 10:16:56 +0300

    Sprint 13. Director feature was added

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,6 +14,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/films")
